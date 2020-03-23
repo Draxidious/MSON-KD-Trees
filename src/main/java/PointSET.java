8de520638010be96f9e9f8ce49d5dabd81de4da5
Jordan Muniz <munizj21@trinityprep.org>
2020-03-23 16:45:53 -0400

    feat: Implement PointSET methods

--- src/main/java/PointSET.java
+++ src/main/java/PointSET.java
@@ -20,46 +20,77 @@
 
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.SET;
+import edu.princeton.cs.algs4.StdDraw;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 
 public class PointSET {
-
+	SET<Point2D> pointSet;
 	// construct an empty set of points
 	public PointSET() {
-
+		pointSet = new SET<>();
 	}
 	// is the set empty? 
 	public boolean isEmpty() {
-		return false;
+		return pointSet.isEmpty();
 	}
 	// number of points in the set 
 	public int size() {
-		return 0;
+		return pointSet.size();
 	}
 
 	// add the point to the set (if it is not already in the set)
 	public void insert(Point2D p) {
-		
+		pointSet.add(p);
 	}
 
 	// does the set contain point p? 
 	public boolean contains(Point2D p) {
-		return false;
+		return pointSet.contains(p);
 	}
 
 	// draw all points to standard draw 
 	public void draw() {
-		
+		for (Point2D next : pointSet) {
+			StdDraw.point(next.x(),next.y());
+		}
 	}
 
 	// all points that are inside the rectangle
 	public Iterable<Point2D> range(RectHV rect) {
-		return null;
+		//lopp through all the points in pointSet
+			//if rect contains it then add it to a list
+		//return list
+		Iterator<Point2D> iter = pointSet.iterator();
+		ArrayList<Point2D> ret = new ArrayList<>();
+		while (iter.hasNext())
+		{
+			Point2D next = iter.next();
+			if(rect.contains(next))
+			{
+				ret.add(next);
+			}
+		}
+
+		return ret;
 	}
 
 	// a nearest neighbor in the set to point p; null if the set is empty 
 	public Point2D nearest(Point2D p) {
-		return null;
+		if(isEmpty()) return null;
+		Point2D nearestPoint = pointSet.min();
+		double curdist = nearestPoint.distanceTo(p);
+		for (Point2D next : pointSet) {
+			if (next.distanceTo(p) < curdist) {
+				curdist = next.distanceTo(p);
+				nearestPoint = next;
+			}
+		}
+		return nearestPoint;
 	}
 
 	// unit testing of the methods (optional) 
