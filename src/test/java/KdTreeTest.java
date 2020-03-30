fbf1b5f1e600ba0bf25cc346f39b67f63093fb20
Jordan Muniz <munizj21@trinityprep.org>
2020-03-30 14:43:25 -0400

    test: Add tests for KDTree for all methods

--- src/test/java/KdTreeTest.java
+++ src/test/java/KdTreeTest.java
@@ -1,42 +1,73 @@
 import static org.junit.Assert.*;
 
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.StdDraw;
 import org.junit.Before;
 import org.junit.Test;
 
-public class KdTreeTest {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
+public class KdTreeTest {
+  KdTree kd;
   @Before
   public void setUp() throws Exception {
-  }
-
-  @Test
-  public void testIsEmpty() {
-    fail("Not yet implemented");
-  }
-
-  @Test
-  public void testSize() {
-    fail("Not yet implemented");
+    kd = new KdTree();
   }
 
   @Test
   public void testInsert() {
-    fail("Not yet implemented");
+    //basic insert
+    assertTrue(kd.isEmpty());
+    kd.insert(new Point2D(.2,.3));
+    assertEquals(kd.size(),1);
+    assertFalse(kd.isEmpty());
+    assertTrue(kd.contains(new Point2D(.2,.3)));
+
+    //real insert test
+    kd.insert(new Point2D(.1,.5));
+    kd.insert(new Point2D(.2,.4));
+    kd.insert(new Point2D(.7,.6));
+
+    assertTrue(kd.contains(new Point2D(.1,.5)));
+    assertTrue(kd.contains(new Point2D(.2,.4)));
+    assertTrue(kd.contains(new Point2D(.2,.3)));
+    assertTrue(kd.contains(new Point2D(.7,.6)));
+
+    assertEquals(kd.size(),4);
   }
 
-  @Test
-  public void testContains() {
-    fail("Not yet implemented");
-  }
 
   @Test
   public void testRange() {
-    fail("Not yet implemented");
+    ArrayList<Point2D> arr = new ArrayList<>();
+    arr.add(new Point2D(.3,.4));
+    kd.insert(new Point2D(.3,.4));
+    assertEquals(arr,kd.range(new RectHV(0.0,0.0,1.0,1.0)));
+
+    kd.insert(new Point2D(.2,.6));
+    kd.insert(new Point2D(.5,.2));
+    kd.insert(new Point2D(.8,.1));
+    System.out.println(kd.range(new RectHV(0.4,0.5,1.0,1.0)));
+
   }
 
   @Test
   public void testNearest() {
-    fail("Not yet implemented");
+    // tree example
+    kd.insert(new Point2D(.7,.2));
+    kd.insert(new Point2D(.5,.4));
+    kd.insert(new Point2D(.9,.6));
+    kd.insert(new Point2D(.2,.3));
+    kd.insert(new Point2D(.4,.7));
+
+    assertEquals(kd.nearest(new Point2D(.7,.2)),new Point2D(.7,.2));
+    assertEquals(kd.nearest(new Point2D(.7,.3)),new Point2D(.7,.2));
+    assertEquals(kd.nearest(new Point2D(.2,.3)),new Point2D(.2,.3));
+    assertEquals(kd.nearest(new Point2D(.2,.4)),new Point2D(.2,.3));
+    System.out.println(kd.nearest(new Point2D(.8,.2)));
+
   }
 
 }
